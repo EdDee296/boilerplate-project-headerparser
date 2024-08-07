5941ac62b038389e6fa7ff67282465b46190f526
EdDee296 <109675085+EdDee296@users.noreply.github.com>
2024-08-07 09:47:59 +0700

    finish

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  var ipadress = req.ip;
+  var language = req.acceptsLanguages();
+  var software=req.get('User-Agent');
+   res.json({
+   ipadress: ipadress,
+   language:language[0],
+   software:software
+   });
+  });
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
